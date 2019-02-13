70845ee87941150931dfb1f141fdb9084df0fefe
F. Perdomo <perdomofacundo@hotmail.com>
2019-02-12 23:41:21 -0300

    Fix Datatable

--- Social.Logins.Web/wwwroot/js/site.js
+++ Social.Logins.Web/wwwroot/js/site.js
@@ -647,14 +647,9 @@ function blockUICustom() {
     $.blockUI({
         css: {
             border: 'none',
-            padding: '15px',
-            backgroundColor: '#000',
-            '-webkit-border-radius': '10px',
-            '-moz-border-radius': '10px',
-            opacity: .5,
-            color: '#fff',
-            fontSize: '10px'
+            backgroundColor: 'transparent'
         },
+        message: '<img src="/css/images/busy.gif"/>',
         baseZ: 2000
     }); 
 }
